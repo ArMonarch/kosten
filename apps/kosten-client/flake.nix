165a93eda2d51a8b6d2c635ac11842e7aad2c4b3
ArMonarch <praffulthapa12@gmail.com>
2026-01-16 17:07:11 +0545

    feat(client): initial kosten client project setup

---
+++ apps/kosten-client/flake.nix
@@ -0,0 +1,35 @@
+{
+  description = "A Nix-flake-based JavaScript/TypeScript(feat. Bun) Development Environment.";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
+
+    # defines system that this flake supports
+    systems.url = "github:nix-systems/default-linux";
+
+    # Powered by
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
+  };
+
+  outputs = {flake-parts, ...} @ inputs:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      systems = import inputs.systems;
+
+      perSystem = {pkgs, ...}: {
+        devShells.default = pkgs.mkShellNoCC {
+          packages = with pkgs; [
+            bun
+            nodejs_latest
+          ];
+          shellHook = ''
+            echo "Initialized JavaScript/TypeScript(feat. Bun) Development Environment"
+            echo "  ├── bun: $(bun --version)"
+            echo "  └── node:  $(node --version)"
+          '';
+        };
+      };
+    };
+}
